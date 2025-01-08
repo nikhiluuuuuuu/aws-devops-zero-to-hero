5c1be604376baa4c4b3f040c5ecba455b05c029e
nikhiluuuuuuu <nikhiluu9696@gmail.com>
2025-01-08 21:37:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-set -e
 
-# Pull the Docker image from Docker Hub
-docker pull nikhil976/simple-python-flask-app
+# Pull the latest image
+echo "Using default tag: latest"
+docker pull nikhil976/simple-python-flask-app:latest
 
-# Run the Docker image as a container
-docker run -d -p 8080:5000 nikhil976/simple-python-flask-app:latest
-      
+# Check if port 5000 is free, otherwise map to a different port
+if ! lsof -i :5000; then
+  echo "Port 5000 is free. Starting container..."
+  docker run -d -p 5000:5000 nikhil976/simple-python-flask-app:latest
+else
+  echo "Port 5000 is occupied, using port 5001 instead..."
+  docker run -d -p 5001:5000 nikhil976/simple-python-flask-app:latest
+fi
